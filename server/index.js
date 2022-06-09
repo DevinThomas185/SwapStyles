463bdb554be7e143cf4e9f9bc760029558f0dd4a
Joshua Hockley <jmh20@ic.ac.uk>
2022-06-09 11:02:38 +0100

    fix typo

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 // backend api
 app.get('/api', (req, res) => {
