aae687c78084481431816e7d95f91099f52fe446
Raaif Hadadi <hraaif@yahoo.com>
2022-06-09 19:20:39 +0100

    Implement landing page

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
 
     render() {
         return (
-            <div>
-                HOME
+            <div class="home-container">
+                <Link to="/findEvent" class="EventF" ><div class="home-font"> <h1> Find Events </h1> </div>  </Link>
+                <Link to="/createEvent" class="EventC"><div class="home-font"> <h1 class="home-font"> Create an Event </h1> </div> </Link>
+                <Link to="/listClothes" class="ClothesL"><div class="home-font"> C </div> </Link>
+                <Link to="/findClothes" class="ClothesF"> <div class="home-font"> <h1> Find Clothes </h1> </div> </Link>
             </div>
         )
     }
