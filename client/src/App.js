92747289289d74eaf605157e8257343fbae53390
Adam Alilou <aa1320@ic.ac.uk>
2022-06-10 11:22:23 +0100

    milestone 2 demo

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Fashion Swap!!</h1>
       {/* <p>Backend says... {text}</p> */}
       <CreateEvent />
       {/* <Listing /> */}
