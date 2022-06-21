19102a8b8726d8cce55b920a0bd4d9660f855ba2
Raaif Hadadi <hraaif@yahoo.com>
2022-06-21 16:52:56 +0100

    convert shop to function

--- client/src/Pages/Shop.js
+++ client/src/Pages/Shop.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Product from '../Components/Product';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/esm/Container';
@@ -7,60 +7,51 @@ import SearchBar from '../Components/SearchBar';
 import Filters from '../Components/Filters';
 
 
-class Shop extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            products: []
-        };
-        this.getProducts = this.getProducts.bind(this);
-        this.available = this.available.bind(this);
-    }
+function Shop() {
+
+    const [products, setProducts] = useState([]);
 
 
-    async getProducts(query) {
+    async function getProducts(query) {
         const res = await fetch(`/api/getProducts?q=${query}`);
         console.log(res);
         const data = await res.json();
         data.reverse()
         console.log(data);
-        this.setState({
-            products: data
-        });
+        setProducts(data);
     }
 
-    async available(product) {
+    async function available(product) {
         if (product.online) {
             console.log("online")
             return ("online")
         } else {
             return fetch(`/api/getEvent/?id=${product.eventid}`)
-            .then(res => res.json())
-            .then(data => { 
-                console.log(data.name);
-                return ("at " + data.name) 
-            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data.name);
+                    return ("at " + data.name)
+                })
         }
     }
 
 
-    render() {
-        return (
-            <div>
-                <SearchBar getResults={this.getProducts} />
-                <Filters />
-                <Container>
-                    <Row>
-                        {this.state.products.map(item => (
-                            <Col key={item.id}>
-                                <Product product={item} available={this.available}/>
-                            </Col>
-                        ))}
-                    </Row>
-                </Container>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <SearchBar getResults={getProducts} />
+            <Filters />
+            <Container>
+                <Row>
+                    {products.map(item => (
+                        <Col key={item.id}>
+                            <Product product={item} available={available(item)} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </div>
+    );
+
 }
 
 export default Shop;
