948cc240a440e3cbcfa351d89d910541f849b59b
Devin Thomas <devin.thomas20@imperial.ac.uk>
2022-06-23 15:27:44 +0100

    [dt120] - Dark carousels and no title

--- client/src/Components/ItemPreviews.js
+++ client/src/Components/ItemPreviews.js
@@ -5,16 +5,16 @@ import Carousel from 'react-bootstrap/Carousel';
 class ItemPreviews extends React.Component {
     render() {
         return (
-            <Carousel interval={10000}>
+            <Carousel 
+            interval={10000}
+            variant="dark"
+            >
                 {this.props.items.map(item => (
                     <Carousel.Item key={item.id}>
                         
                         <a href={"/product/" + item.id}>
                             <img src={item.url} alt={item.description} className='mx-auto d-block' height={200}/>
                         </a>
-                        <Carousel.Caption>
-                            <p>{item.title}</p>
-                        </Carousel.Caption>
                     </Carousel.Item>
                 ))}
             </Carousel>
